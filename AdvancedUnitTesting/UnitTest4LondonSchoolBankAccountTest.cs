4095811336c47adcb7229952498b75ed9d417cfe
Boris Blokland <boris.blokland@alten.nl>
2025-05-26 08:13:51 +0200

    cleanup classical vs londonschool tests

---
+++ AdvancedUnitTesting/UnitTest4LondonSchoolBankAccountTest.cs
@@ -0,0 +1,76 @@
+using NUnit.Framework;
+using System;
+
+namespace UnitTest4AdvancedCustomerStore;
+
+[TestFixture]
+public class BankAccountTests
+{
+    [Test]
+    public void Constructor_ShouldSetInitialBalance()
+    {
+        // Arrange
+        var initialBalance = 100.0;
+
+        // Act
+        var bankAccount = new BankAccount(initialBalance);
+
+        // Assert
+        Assert.That(bankAccount.CurrentBalance, Is.EqualTo(initialBalance));
+    }
+
+    [Test]
+    public void Deposit_ShouldIncreaseCurrentBalance()
+    {
+        // Arrange
+        var bankAccount = new BankAccount(50.0);
+        var amountToDeposit = 25.0;
+
+        // Act
+        bankAccount.Deposit(amountToDeposit);
+
+        // Assert
+        Assert.That(bankAccount.CurrentBalance, Is.EqualTo(75.0));
+    }
+
+    [Test]
+    public void Withdraw_ShouldDecreaseCurrentBalance()
+    {
+        // Arrange
+        var bankAccount = new BankAccount(100.0);
+        var amountToWithdraw = 30.0;
+
+        // Act
+        bankAccount.Withdraw(amountToWithdraw);
+
+        // Assert
+        Assert.That(bankAccount.CurrentBalance, Is.EqualTo(70.0));
+    }
+
+    [Test]
+    public void Withdraw_ShouldThrowInvalidOperationExceptionWhenInsufficientFunds()
+    {
+        // Arrange
+        var bankAccount = new BankAccount(20.0);
+        var amountToWithdraw = 50.0;
+
+        // Act & Assert
+        var ex = Assert.Throws<InvalidOperationException>(() => bankAccount.Withdraw(amountToWithdraw));
+        Assert.That(ex.Message, Is.EqualTo("Insufficient funds"));
+        Assert.That(bankAccount.CurrentBalance, Is.EqualTo(20.0)); // Ensure balance remains unchanged
+    }
+
+    [Test]
+    public void Withdraw_ShouldAllowWithdrawalOfExactBalance()
+    {
+        // Arrange
+        var bankAccount = new BankAccount(50.0);
+        var amountToWithdraw = 50.0;
+
+        // Act
+        bankAccount.Withdraw(amountToWithdraw);
+
+        // Assert
+        Assert.That(bankAccount.CurrentBalance, Is.EqualTo(0.0));
+    }
+}
---
+++ AdvancedUnitTesting/UnitTest4LondonSchoolStoreTest.cs
@@ -0,0 +1,134 @@
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTest4AdvancedCustomerStore;
+
+[TestFixture]
+public class StoreTests
+{
+    [Test]
+    public void AddInventory_ShouldIncreaseInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Shampoo;
+        var quantity = 10;
+
+        // Act
+        store.AddInventory(product, quantity);
+
+        // Assert
+        Assert.That(store.GetInventory(product), Is.EqualTo(quantity));
+    }
+
+    [Test]
+    public void AddInventory_ShouldAddMultipleTimesForSameProduct()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Book;
+        var quantity1 = 5;
+        var quantity2 = 3;
+
+        // Act
+        store.AddInventory(product, quantity1);
+        store.AddInventory(product, quantity2);
+
+        // Assert
+        Assert.That(store.GetInventory(product), Is.EqualTo(quantity1 + quantity2));
+    }
+
+    [Test]
+    public void GetInventory_ShouldReturnZeroForNonExistentProduct()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Book;
+
+        // Act & Assert
+        Assert.That(store.GetInventory(product), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void HasEnoughInventory_ShouldReturnTrueIfEnoughInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Shampoo;
+        store.AddInventory(product, 5);
+
+        // Act & Assert
+        Assert.That(store.HasEnoughInventory(product, 3), Is.True);
+    }
+
+    [Test]
+    public void HasEnoughInventory_ShouldReturnTrueIfExactlyEnoughInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Shampoo;
+        store.AddInventory(product, 5);
+
+        // Act & Assert
+        Assert.That(store.HasEnoughInventory(product, 5), Is.True);
+    }
+
+    [Test]
+    public void HasEnoughInventory_ShouldReturnFalseIfNotEnoughInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Shampoo;
+        store.AddInventory(product, 5);
+
+        // Act & Assert
+        Assert.That(store.HasEnoughInventory(product, 7), Is.False);
+    }
+
+    [Test]
+    public void RemoveInventory_ShouldDecreaseInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Book;
+        store.AddInventory(product, 10);
+        var quantityToRemove = 4;
+
+        // Act
+        store.RemoveInventory(product, quantityToRemove);
+
+        // Assert
+        Assert.That(store.GetInventory(product), Is.EqualTo(6));
+    }
+
+    [Test]
+    public void RemoveInventory_ShouldThrowExceptionWhenNotEnoughInventory()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Book;
+        store.AddInventory(product, 3);
+        var quantityToRemove = 5;
+
+        // Act & Assert
+        var ex = Assert.Throws<Exception>(() => store.RemoveInventory(product, quantityToRemove));
+        Assert.That(ex.Message, Is.EqualTo("Not enough inventory"));
+        Assert.That(store.GetInventory(product), Is.EqualTo(3)); // Ensure inventory remains unchanged
+    }
+
+    [Test]
+    public void RemoveInventory_ShouldThrowExceptionWhenProductDoesNotExist()
+    {
+        // Arrange
+        var store = new Store();
+        var product = Product.Book;
+        var quantityToRemove = 5;
+
+        // Act & Assert
+        var ex = Assert.Throws<Exception>(() => store.RemoveInventory(product, quantityToRemove));
+        Assert.That(ex.Message, Is.EqualTo("Not enough inventory"));
+        Assert.That(store.GetInventory(product), Is.EqualTo(0)); // Ensure inventory remains unchanged
+    }
+}
